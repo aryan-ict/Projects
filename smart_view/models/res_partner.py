a72748fd4004def15897c3e7bee56b3b3c34f205
aryan-ict <aryan@icreativetechnolabs.com>
2022-03-15 18:36:54 +0530

    [IMP]Performed inheritance in res.partner added birth_date and age fields and tried _compute function for age count

--- smart_view/models/res_partner.py
+++ smart_view/models/res_partner.py
@@ -1,5 +1,6 @@
 """This model is to Inherit fields in Res Partner Model"""
 from odoo import models, fields, api
+import datetime
 
 
 class ResPartner(models.Model):
@@ -7,6 +8,8 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     customer_ref = fields.Char(string="Customer Reference")
+    birth_date = fields.Date(string="Birth Date")
+    age = fields.Integer(string="Age")
 
     def name_get(self):
         """Function for getting the list of full names."""
@@ -18,8 +21,18 @@ class ResPartner(models.Model):
 
     @api.model
     def _name_search(self, name, args=None, operator='ilike', limit=100):
+        """Function for Name Search Method."""
         args = args or []
         domain = []
         if name:
             domain = ['|', '|', ('name', operator, name), ('phone', operator, name), ('email', operator, name)]
         return self._search(domain + args, limit=limit)
+
+    @api.depends('birth_date')
+    def _compute_calculate_age(self):
+        today_date = datetime.date.today()
+        for rec in self:
+            if rec.birth_date:
+                bdate = fields.Datetime.to_datetime(rec.birthdate).date()
+                rec.age = str(int((today_date - bdate).days / 365))
+                print("Date------------\n", bdate)
