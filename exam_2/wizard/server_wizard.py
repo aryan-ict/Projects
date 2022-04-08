db1138a0bbadd7df220eff645ac30283cb026513
aryan-ict <aryan@icreativetechnolabs.com>
2022-04-08 18:10:50 +0530

    [IMP]Improved function for 4th task.

--- exam_2/wizard/server_wizard.py
+++ exam_2/wizard/server_wizard.py
@@ -6,25 +6,22 @@ class ServerWizard(models.TransientModel):
     """This class contains fields for wizard"""
     _name = "server.wizard"
 
-    sale_order_ids = fields.One2many('server.wizard.otm', 'product_qty', string="Sale Order")
+    sale_order_ids = fields.One2many('server.wizard.otm', 'test_id', string="Sale Order")
 
     def create_so(self):
         """Function to get order lines on the
         click of the button in wizard."""
-        for rec in self:
-            new_lines = []
-            for res in rec.sale_order_ids:
-                new_lines.append((0, 0, {
-                    'product_id': res.product_id.id,
-                    'product_uom': res.price_unit,
-                    'product_uom_qty': res.product_qty
-                }))
-            print("-----------------------", self._context)
-            vals = self.env['sale.order'].create({'order_line': new_lines})
-            print("-------------------------", new_lines)
+        new_lines = []
+        for res in self.sale_order_ids:
+            new_lines.append((0, 0, {
+                'product_id': res.product_id.id,
+                'product_uom_qty': res.product_qty
+            }))
+        print("-----------------------", self._context)
+        for rec in self._context.get('active_ids'):
+            vals = self.env['sale.order'].create({'partner_id': rec, 'order_line': new_lines})
             print("-------------------------", vals)
-            print("-------------------------", )
-            return vals
+        print("-------------------------", new_lines)
 
 
 class ServerWizardOtm(models.TransientModel):
@@ -33,5 +30,6 @@ class ServerWizardOtm(models.TransientModel):
     _description = 'Server Wizard One2many'
 
     product_id = fields.Many2one('product.product', string="Product")
-    product_qty = fields.Integer(string="Product Quantity")
+    product_qty = fields.Float(string="Product Quantity")
     price_unit = fields.Float(string="Unit Price", related="product_id.list_price")
+    test_id = fields.Many2one('server.wizard')
