1522e6e7807fb10d4df0a34162116ec5a7318932
aryan-ict <aryan@icreativetechnolabs.com>
2022-06-24 18:37:16 +0530

    [FIX]Trying new approach for apply_promotional_discount function.

--- promotional_discount/models/sale_order.py
+++ promotional_discount/models/sale_order.py
@@ -8,28 +8,40 @@ class SaleOrder(models.Model):
     def apply_promotional_discount(self):
         pro_discount = self.env['product.product'].search([('default_code', '=', 'DISC')])
         promo_disc = self.env['promotional.discount'].search(
-            [('start_date', '<=', self.date_order), ('end_date', '>', self.date_order)])
+            [('start_date', '<=', self.date_order), ('end_date', '>=', self.date_order),
+             ('minimum_order_amount', '<=', self.amount_total)])
         print("-----------------------------discount", pro_discount)
-        new_line = []
+        print("-----------------------------promotional", promo_disc)
+        percentage_discount = []
+        fixed_discount = []
         for rec in promo_disc:
+            print("============================rec", rec)
             if rec.discount_type == 'fixed':
-                new_line.append((0, 0, {
-                    'product_id': pro_discount.id,
-                    'price_unit': "-" + str(rec.fixed_discount)
-                }))
-                self.update({
-                    'order_line': new_line,
-                })
+                price = rec.fixed_discount
+                fixed_discount.append(price)
+
+                # new_line.append((0, 0, {
+                #     'product_id': pro_discount.id,
+                #     'price_unit': "-" + str(rec.fixed_discount)
+                # }))
+                print("==============================price", price)
+                print("---------------------fixed_discount", fixed_discount)
+            # print("=============================new_line", new_line)
             elif rec.discount_type == 'percentage':
-                price = self.order_line.price_unit - (self.order_line.price_unit * (rec.discount / 100.0))
-                print("-----------------price", price)
-                new_line.append((0, 0, {
-                    'product_id': pro_discount.id,
-                    'price_unit': rec.discount
-                }))
-                self.update({
-                    'order_line': new_line,
-                    'amount_total': price
-                })
-                print("=============================new_line", new_line)
+                for res in self.order_line:
+                    price = (res.price_unit * (rec.discount / 100.0))
+                    percentage_discount.append(price)
+                    print("-----------------price", price)
+                print("-------------------------percentage_discount", percentage_discount)
+        # new_line.append((0, 0, {
+        #     'product_id': pro_discount.id,
+        #     'price_unit': "-" + str(rec.discount)
+        # }))
+        # print("=============================new_line", new_line)
+
+    # self.write({
+    #     'order_line': {'product_id': pro_discount.id,
+    #                    'price_unit':}
+    #     'amount_total': price
+    # })
         print("----------------------------", self.tax_totals_json)
